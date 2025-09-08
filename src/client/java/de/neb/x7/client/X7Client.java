63df3f147e2c4971f311cb3a087be98413a4b2b8
NebPLL <b.hager.im25@stud.bbbaden.ch>
2025-09-08 11:42:12 +0200

    Init Commit

---
+++ src/client/java/de/neb/x7/client/X7Client.java
@@ -0,0 +1,10 @@
+package de.neb.x7.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class X7Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/de/neb/x7/X7.java
@@ -0,0 +1,10 @@
+package de.neb.x7;
+
+import net.fabricmc.api.ModInitializer;
+
+public class X7 implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}

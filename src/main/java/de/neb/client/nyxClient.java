70c68a209a15957c17e0b0dd90410ae35138222a
NebPLL <b.hager.im25@stud.bbbaden.ch>
2025-09-19 13:34:19 +0200

    Inital Commit

---
+++ src/main/java/de/neb/client/nyxClient.java
@@ -0,0 +1,10 @@
+package de.neb.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class nyxClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/de/neb/nyx.java
@@ -0,0 +1,10 @@
+package de.neb;
+
+import net.fabricmc.api.ModInitializer;
+
+public class nyx implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
